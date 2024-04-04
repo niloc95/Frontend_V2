5a80a35544da08a3e2e3a134f0784aac341d3ea4
Nilo <nilo.cara@gmail.com>
2024-04-04 18:01:00 +0200

    404 Updates5

--- fesite/views.py
+++ fesite/views.py
@@ -128,6 +128,10 @@ def tailwind(request):
     context = common_context()
     return render(request, 'pages/tailwind.html', context)
 
+# def nof(request):
+#     context = common_context()
+#     return render(request, '404.html', context)
+
 def not_found_404_view(request, exception):
     context = common_context()
     return render(request, '404.html', {}, status=404, context=context)
